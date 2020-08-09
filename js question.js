e590d24b7e19837849f2f9b08321699d3e12155a
ebrahimpaki <ebrahimpaki@gmail.com>
2020-08-09 07:23:30 -0700

    initial commit

---
+++ js question.js
@@ -0,0 +1,31 @@
+
+
+// Arrray of object
+const quiz = [
+    {
+        q: 'Q. Who was the inventor of Ctrl+C (copy), Ctrl+V (Paste ) and Ctrl+X (Cut)?',
+        options:['Bill Gates','Larry Tesler','Christopher Latham Sholes','David Sundstrand'],
+        answer:1
+    },
+    {
+    q: ' _______ is known as leap day or leap year day:',
+    options:[' Feb 29','Feb 28','March 01','Feb 01'],
+    answer:0
+},
+{
+    q:  "The World’s first camel hospital is located in:",
+    options:['Dubai, UAE','Jaddah, Saudi Arabia',' Mascat, Oman','None of These'],
+    answer:0
+},
+{
+    q: ' In Pakistan the Monsoon season lasts from:',
+    options:['April to may','May to June',' June to December','July to September'],
+    answer:3
+},
+{
+    q:'What is 22 + 6 ?',
+    options:['99','56','16','28'],
+    answer:3
+}
+
+]
